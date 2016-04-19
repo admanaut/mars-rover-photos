3be40f6f5e08c15660033cdb0792365ba49d5344
Adrian Marina <adrianmarina.adrian@gmail.com>
2016-04-19 03:57:09 +0100

    add lein build config to project file

--- project.clj
+++ project.clj
@@ -9,4 +9,11 @@
                  [compojure "1.5.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]]
+  :main ^:skip-aot mars-rover-photos.core
+  :uberjar-name "mars-rover-photos.jar"
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler mars-rover-photos.core/application}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}
+             :uberjar {:aot :all}})
